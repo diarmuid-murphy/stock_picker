369e2f1eb8a230301900ee3ce833d2d342fd9855
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-04-21 22:20:06 -0500

    complete

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,38 +1,25 @@
 def stock_picker(stock_prices)
-	buy_position = 0
-	sell_position = 0
-	highest_increase = 0
+	best_buy = 0
+	best_sell = 0
+	max_profit = 0
 	
-	price_day_map = Hash.new(0)
-	
-	stock_prices.each do |price|
-		price_day_map[price] = stock_prices.index(price)
+	(0..(stock_prices.length - 2)).each do |buy_date|
+		((buy_date + 1)..stock_prices.length - 1).each do |sell_date|
+			
+			price_difference = stock_prices[sell_date] - stock_prices[buy_date]
+			
+			if price_difference > max_profit
+				best_buy = buy_date
+				best_sell = sell_date
+				max_profit = price_difference
+			end
+		end
 	end
 	
-	price_low_to_high = price_day_map.sort
-	price_high_to_low = price_low_to_high.reverse
-	
-	# puts price_day_map.inspect}
-	puts "The lowest price, day: #{price_low_to_high.inspect}"
-	puts "The highest price, day: #{price_high_to_low.inspect}"
-	
-	price_low_to_high.each do |price, day|
-		# print day.to_s + " "
-		# puts "On day #{day} the price is #{price}"
-	end
-	
-	print "\n"
-	
-	# print price_high_to_low.max
-	puts price_high_to_low.max[0]
-	puts price_high_to_low[0][1]
-	
-	while(price_high_to_low.max[0]) == (price_high_to_low[0][0]) do
-		price_high_to_low = price_high_to_low[1..-1]
-		print price_high_to_low.inspect
-	end
+	puts [best_buy, best_sell].inspect
+	puts "For a profit of $#{stock_prices[best_sell]} - $#{stock_prices[best_buy]} == $#{max_profit}"
 end
 
-# stock_picker([3,6,9,15,8,6,1,10])
+stock_picker([17,3,6,9,15,8,6,1,10])
 # stock_picker([35,1,2,3,4,5,8,9,9, 1])
-stock_picker([35,10,9,8,7,6,5,4,5,3,2,1])
+# stock_picker([35,10,9,8,7,6,5,4,5,3,2,1])
